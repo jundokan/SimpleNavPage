bfdf6a077213c54f0601d00ad3606052a7a4f3cb
Sergii Kuzmin <jundokan2000@gmail.com>
2015-10-05 05:09:42 +0300

    Added HomePage SecondPage files and button handlees

---
+++ NavPage/NavPage/NavPage/HomePage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace NavPage
+{
+    class HomePage : ContentPage
+    {
+        Button homeButton;
+        public HomePage()
+        {
+            Title = "Hierarchical Navigation";
+            Label homeLabel = new Label
+            {
+                Text = "Home Page",
+                FontSize = 40
+            };
+
+            homeButton = new Button
+            {
+                Text = "Go to Second Page"
+            };
+
+            Button button = new Button { Text = "Show Alert" };
+
+            Button buttonBoolean = new Button { Text = "Show Boolean Alert" };
+
+            button.Clicked += async (sender, e) =>
+            {
+                await DisplayAlert("Hey", "You really should know about this.", "OK");
+            };
+            
+            buttonBoolean.Clicked += async (sender, e) =>
+            {
+                Boolean answer = await DisplayAlert("Start",
+                "Are you ready to begin?", "Yes", "No");
+            };
+
+            homeButton.Clicked += async (sender, args) =>
+            await Navigation.PushAsync(new SecondPage(), true);
+
+            StackLayout stackLayout = new StackLayout
+            {
+                Children =
+                {
+                    homeLabel,
+                    homeButton,
+                    button,
+                    buttonBoolean
+                }
+            };
+
+            this.Content = stackLayout;
+        }
+    }
+}
---
+++ NavPage/NavPage/NavPage/SecondPage.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+
+namespace NavPage
+{
+    internal class SecondPage : ContentPage
+    {
+        public SecondPage()
+        {
+            Title = "Hierarchical Navigation";
+            Label homeLabel = new Label
+            {
+                Text = "Second Page",
+                FontSize = 40
+            };
+            var stackLayout = new StackLayout
+            {
+                Children = { homeLabel }
+            };
+            this.Content = stackLayout;
+        }
+    }
+}
